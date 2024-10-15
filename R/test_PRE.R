5cb4c64418733a1911f7c7a9d3949e2c93717e0b
qyaozh <qyaozh@gmail.com>
2024-10-15 20:34:29 +0800

    Two useges of test_PRE

--- R/test_PRE.R
+++ R/test_PRE.R
@@ -2,6 +2,11 @@
 #'
 #' @param fitc The result of `lm()` of the Compact model (Model C).
 #' @param fitA The result of `lm()` of the Augmented model (Model A).
+#' @param n Sample size of the Model C or Model A. Model C and Model A must use the same sample, and hence have the same sample size.
+#' @param PC The number of parameters in Model C.
+#' @param PA The number of parameters in Model A. PA must be larger than PC.
+#' @param SSEC The Sum of Squared Errors of Model C.
+#' @param SSEA The Sum of Squared Errors of Model A.
 #'
 #' @details Please refer to Judd et al. (2017) for details.
 #' @references Judd, C. M., McClelland, G. H., & Ryan, C. S. (2017). *Data analysis: A model comparison approach to regression, ANOVA, and beyond*. Routledge.
@@ -16,23 +21,31 @@
 #' fitC <- lm(y ~ 1, dat)
 #' fitA <- lm(y ~ x, dat)
 #' test_PRE(fitC, fitA)
-test_PRE <- function(fitc, fitA) {
-  n = length(fitc$residuals)
-  if (n != length(fitA$residuals))
-    stop("sample size of Model C does not equal to Model A.")
-  PC = length(fitc$coefficients)
-  PA = length(fitA$coefficients)
-  if (PC >= PA)
-    stop("Model C has more parameters than Model A.")
-  SSEC = sum((fitc$residuals)^2)
-  SSEA = sum((fitA$residuals)^2)
-  SSR = SSEC - SSEA
-  PRE = 1 - SSEA/SSEC
-  df1 = PA - PC
-  df2 = n - PA
-  F = (SSR/df1)/(SSEA/df2)
-  p = stats::pf(F, df1, df2, lower.tail = FALSE)
-  out = data.frame(SSEC, SSEA, PRE, F, df1, df2, p)
-  row.names(out) <- paste0("Result",row.names(out))
+test_PRE <- function(fitc=NULL, fitA=NULL, n=NULL, PC=NULL, PA=NULL, SSEC=NULL, SSEA=NULL) {
+  if (
+    (sum(sapply(list(fitc, fitA), is.null)) == 0) & (sum(sapply(list(n, PC, PA, SSEC, SSEA), is.null)) != 5) |
+    (sum(sapply(list(fitc, fitA), is.null)) != 0) & (sum(sapply(list(n, PC, PA, SSEC, SSEA), is.null)) == 5) |
+    (sum(sapply(list(fitc, fitA), is.null)) != 2) & (sum(sapply(list(n, PC, PA, SSEC, SSEA), is.null)) == 0) |
+    (sum(sapply(list(fitc, fitA), is.null)) == 2) & (sum(sapply(list(n, PC, PA, SSEC, SSEA), is.null)) != 0)
+    )
+    stop("Provide fitc and fitA, or provide n, PC, PA, SSEC, and SSEA instead.")
+  else if ((sum(sapply(list(fitc, fitA), is.null)) == 0) & (sum(sapply(list(n, PC, PA, SSEC, SSEA), is.null)) == 5)) {
+           n = length(fitc$residuals)
+           if (n != length(fitA$residuals))
+             stop("sample size of Model C must be equal to Model A.")
+           PC = length(fitc$coefficients)
+           PA = length(fitA$coefficients)
+           if (PC >= PA)
+             stop("Model C must has less parameters than Model A.")
+           SSEC = sum((fitc$residuals)^2)
+           SSEA = sum((fitA$residuals)^2)
+           }
+  SSR <- SSEC - SSEA
+  PRE <- 1 - SSEA/SSEC
+  df1 <- PA - PC
+  df2 <- n - PA
+  F <- (SSR/df1)/(SSEA/df2)
+  p <- stats::pf(F, df1, df2, lower.tail = FALSE)
+  out <- data.frame(SSEC, SSEA, PRE, F, df1, df2, p)
   return(out)
 }
--- tests/testthat/test-test_PRE.R
+++ tests/testthat/test-test_PRE.R
@@ -5,4 +5,10 @@ test_that("test_PRE", {
   fitC <- lm(y ~ 1, dat)
   fitA <- lm(y ~ x, dat)
   expect_equal(anova(fitC, fitA)[2,"F"], test_PRE(fitC, fitA)$F)
+  n = 193
+  PC = 1
+  PA = 2
+  SSEC = sum(fitC$residuals^2)
+  SSEA = sum(fitA$residuals^2)
+  expect_equal(test_PRE(fitC, fitA), test_PRE(n = n, PC = PC, PA = PA, SSEC = SSEC, SSEA = SSEA))
 })
