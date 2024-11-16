6a08b8b58c4982a5fb0ac1290b353cd3474210a8
qyaozh <qingyaozhang@outlook.com>
2024-11-17 00:03:40 +0800

    center

--- R/center.R
+++ R/center.R
@@ -3,6 +3,9 @@
 #' @param x A vector.
 #'
 #' @returns A vector centered on x's mean that is computed with NA values stripped.
+#'
+#' @export
+#'
 #' @examples
 #' x <- rnorm(193, 100, 15)
 #' center(x)
