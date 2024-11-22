1fef776dd385720c9b09c8da0b59f0381923828c
qyaozh <qingyaozhang@outlook.com>
2024-11-22 10:55:30 +0800

    shorten it

--- R/Scale.R
+++ R/Scale.R
@@ -1,29 +1,29 @@
 #' Scale a vector
 #'
 #' @param x The original vector.
-#' @param expected_M The expected Mean of the scaled vector.
-#' @param expected_SD The expected Standard Deviation (unit) of the scaled vector.
+#' @param m The expected Mean of the scaled vector.
+#' @param sd The expected Standard Deviation (unit) of the scaled vector.
 #' @param oadvances The distance the Origin of x advances by.
 #'
 #' @return The scaled vector.
 #'
 #' @details
-#' To scale `x`, its origin, or unit (*expected_SD*), or both, could be changed.
+#' To scale `x`, its origin, or unit (*sd*), or both, could be changed.
 #'
-#' If `expected_M` = 0 or `NULL`, and `expected_SD` = `NULL`, `x` would be mean-centered.
+#' If `m` = 0 or `NULL`, and `sd` = `NULL`, `x` would be mean-centered.
 #'
-#' If `expected_M` is a non-zero number, and `expected_SD` = `NULL`, the mean of `x` would be transformed to expected_M.
+#' If `m` is a non-zero number, and `sd` = `NULL`, the mean of `x` would be transformed to m.
 #'
-#' If `expected_M` = 0 or `NULL`, and `expected_SD` = 1, `x` would be standardized to be its z-score with *M* = 0 and *SD* = 1.
+#' If `m` = 0 or `NULL`, and `sd` = 1, `x` would be standardized to be its z-score with *m* = 0 and *m* = 1.
 #'
-#' The standardized score is not necessarily the z-score. If neither `expected_M` nor `expected_SD` is `NULL`,
-#' `x` would be standardized to be a vector whose mean and standard deviation would be `expected_M` and `expected_SD`, respectively.
+#' The standardized score is not necessarily the z-score. If neither `m` nor `sd` is `NULL`,
+#' `x` would be standardized to be a vector whose mean and standard deviation would be `m` and `sd`, respectively.
 #' To standardize `x`, the mean and standard deviation of `x` are needed and computed,
 #' for which the missing values of `x` are removed if any.
 #'
 #' If `oadvances` is not `NULL`,  the origin of `x` will advance with the standard deviation being unchanged.
 #' In this case, `Scale()` could be used to pick points in simple slope analysis for moderation models.
-#' Note that when `oadvances` is not `NULL`, `expected_M` and `expected_SD` must be NULL.
+#' Note that when `oadvances` is not `NULL`, `m` and `sd` must be NULL.
 #'
 #' @export
 #'
@@ -32,27 +32,27 @@
 #' # Mean-center x.
 #' Scale(x)
 #' # Transform the mean of x to 3.
-#' Scale(x, expected_M = 3)
+#' Scale(x, m = 3)
 #' # Transform x to its z-score.
-#' Scale(x, expected_SD = 1)
-#' # Standardize x with M = 100 and SD = 15.
-#' Scale(x, expected_M = 100, expected_SD = 15)
+#' Scale(x, sd = 1)
+#' # Standardize x with m = 100 and sd = 15.
+#' Scale(x, m = 100, sd = 15)
 #' # The origin of x advances by 3.
 #' Scale(x, oadvances = 3)
 
-Scale <- function(x, expected_M = NULL, expected_SD = NULL, oadvances = NULL) {
+Scale <- function(x, m = NULL, sd = NULL, oadvances = NULL) {
 
   stopifnot(is.numeric(x))
 
   if (!is.null(oadvances)) {
-    stopifnot(is.numeric(x), is.null(expected_M), is.null(expected_SD))
+    stopifnot(is.numeric(x), is.null(m), is.null(sd))
     x - oadvances
   }
   else {
-    if (is.null(expected_M)) expected_M <- 0
-    else stopifnot(is.numeric(expected_M))
-    if (is.null(expected_SD)) expected_SD <- stats::sd(x, na.rm = TRUE)
-    else stopifnot(is.numeric(expected_SD))
-    expected_M + (x - mean(x, na.rm = TRUE))/stats::sd(x, na.rm = FALSE)*expected_SD
+    if (is.null(m)) m <- 0
+    else stopifnot(is.numeric(m))
+    if (is.null(sd)) sd <- stats::sd(x, na.rm = TRUE)
+    else stopifnot(is.numeric(sd))
+    m + (x - mean(x, na.rm = TRUE))/stats::sd(x, na.rm = FALSE)*sd
   }
 }
