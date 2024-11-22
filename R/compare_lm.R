5a76a1bcc123a2cdb7d9583600315246d8dc285f
qyaozh <qingyaozhang@outlook.com>
2024-11-22 08:52:01 +0800

    compute once for all dfs

--- R/compare_lm.R
+++ R/compare_lm.R
@@ -14,12 +14,12 @@
 #' The first is giving `compare_lm()` fitC and fitA.
 #' The second is giving *n*, *PC*, *PA*, *SSEC*, and *SSEA*.
 #' The first way is more convenient, and it minimizes precision loss by omitting copying-and-pasting.
-#' Note that the *F*-tests for *PRE* and R-squared change are equivalent.
+#' Note that the *F*-tests for *PRE* and that for R-squared change are equivalent.
 #' Please refer to Judd et al. (2017) for more details about *PRE*, and refer to Aberson (2019) for more details about *f_squared* and post-hoc power.
 #'
-#' @references Judd, C. M., McClelland, G. H., & Ryan, C. S. (2017). *Data analysis: A model Comparison approach to regression, ANOVA, and beyond*. Routledge.
+#' @references Aberson, C. L. (2019). *Applied power analysis for the behavioral sciences*. Routledge.
 #'
-#' Aberson, C. L. (2019). *Applied power analysis for the behavioral sciences*. Routledge.
+#' Judd, C. M., McClelland, G. H., & Ryan, C. S. (2017). *Data analysis: A model Comparison approach to regression, ANOVA, and beyond*. Routledge.
 #'
 #' @return A matrix with 4 rows and 10 columns.
 #' The first row reports information for baseline model (intercept-only model).
@@ -27,11 +27,11 @@
 #' *SSE* (Sum of Squared Errors) and *df* of *SSE* for baseline model, model C,
 #' model A, and change (model A vs. model C) are reported in column 1 and column 2.
 #' The information in the fourth row are all for the change; put differently,
-#' These results quantify the effect of new parameters model A has but model C doesn't.
+#' These results could quantify the effect of one or a set of new parameters model A has but model C doesn't.
 #' If fitC and fitA are not inferior to the intercept-only model,
 #' *R-squared*, *Adjusted R-squared*, *PRE*, *PRE_adjusted*, and *f_squared* for the full model
 #' (compared with the baseline model) are reported for model C and model A.
-#' If model C or model A has at least 1 predictor, *F* -test with *p*,
+#' If model C or model A has at least one predictor, *F* -test with *p*,
 #' and post-hoc power would be computed for the corresponding full model.
 #'
 #' @export
@@ -67,43 +67,55 @@ compare_lm <- function(fitC=NULL, fitA=NULL, n=NULL, PC=NULL, PA=NULL, SSEC=NULL
     (sum(sapply(list(fitC, fitA), is.null)) == 2) & (sum(sapply(list(n, PC, PA, SSEC, SSEA), is.null)) == 0)
     ))
     stop("Provide fitc and fitA, or provide n, PC, PA, SSEC, and SSEA instead.")
+
   else if ((sum(sapply(list(fitC, fitA), is.null)) == 0) & (sum(sapply(list(n, PC, PA, SSEC, SSEA), is.null)) == 5)) {
 
            # Given fitC and fitA, compute SSEC and SSEA
            n = length(stats::resid(fitC))
            if (n != length(stats::resid(fitA)))
              stop("sample size of model C must be equal to model A.")
-           PC = length(stats::coef(fitC))
-           PA = length(stats::coef(fitA))
+
+           PC <- length(stats::coef(fitC))
+           PA <- length(stats::coef(fitA))
+
+           df_M  <- n - 1
+           df_C_M <- PC - 1
+           df_C  <- n - PC
+           df_A_M <- PA - 1
+           df_A  <- n - PA
+
            if (PC >= PA)
              stop("model C must has less parameters than model A.")
+
            SSEC = sum(stats::resid(fitC)^2)
            SSEA = sum(stats::resid(fitA)^2)
 
-           # mean model And R-squared
-           SSE_MEAN <- sum((
+           # SSE of the mean model
+           SSEM <- sum((
              stats::model.response(stats::model.frame(fitC)) - mean(stats::model.response(stats::model.frame(fitC)))
            ) ^ 2)
 
-           if (SSE_MEAN >= SSEC) {
-             PRE_C <- 1 - SSEC/SSE_MEAN
-             PRE_C_adj <- 1 - (1 - PRE_C)*((n - 1)/(n - PC))
+           # model C vs the mean model
+           if (SSEM >= SSEC) {
+             PRE_C <- 1 - SSEC/SSEM
+             PRE_C_adj <- 1 - (1 - PRE_C)*(df_M/df_C)
 
-             R_squared_C <- (SSE_MEAN - SSEC)/SSE_MEAN
-             R_squared_adj_C <- 1 - (SSEC/(n - PC))/(SSE_MEAN/(n - 1))
+             R_squared_C <- (SSEM - SSEC)/SSEM
+             R_squared_adj_C <- 1 - (SSEC/df_C)/(SSEM/df_M)
 
-             f_squared_C <- SSE_MEAN/SSEC - 1
-             lambda_C <- f_squared_C*(n - PC)
+             f_squared_C <- SSEM/SSEC - 1
+             lambda_C <- f_squared_C*df_C
 
              if (PC - 1 > 0) {
-               F_C <- ((SSE_MEAN - SSEC)/(PC - 1))/(SSEC/(n - PC))
-               p_C <- stats::pf(F_C, (PC - 1), (n - PC), lower.tail = FALSE)
+               F_C <- ((SSEM - SSEC)/df_C_M)/(SSEC/df_C)
+               p_C <- stats::pf(F_C, df_C_M, df_C, lower.tail = FALSE)
 
-               power_post_C <- stats::pf(stats::qf(0.95, (PC - 1), (n - PC)), (PC - 1), (n - PC), lambda_C, lower.tail = FALSE)
+               power_post_C <- stats::pf(stats::qf(0.95, df_C_M, df_C), df_C_M, df_C, lambda_C, lower.tail = FALSE)
              }
              else {
                F_C <- NA
                p_C <- NA
+
                power_post_C <- NA
              }
            }
@@ -119,20 +131,20 @@ compare_lm <- function(fitC=NULL, fitA=NULL, n=NULL, PC=NULL, PA=NULL, SSEC=NULL
              power_post_C <- NA
            }
 
-           if (SSE_MEAN >= SSEA) {
-             PRE_A <- 1 - SSEA/SSE_MEAN
-             PRE_A_adj <- 1 - (1 - PRE_A)*((n - 1)/(n - PA))
+           if (SSEM >= SSEA) {
+             PRE_A <- 1 - SSEA/SSEM
+             PRE_A_adj <- 1 - (1 - PRE_A)*(df_M/df_A)
 
-             R_squared_A <- (SSE_MEAN - SSEA)/SSE_MEAN
-             R_squared_adj_A <- 1 - (SSEA/(n - PA))/(SSE_MEAN/(n - 1)) # PRE_adj <- 1 - (1 - PRE)*((n - PC)/(n - PA))
+             R_squared_A <- (SSEM - SSEA)/SSEM
+             R_squared_adj_A <- 1 - (SSEA/df_A)/(SSEM/df_M)
 
-             f_squared_A <- SSE_MEAN/SSEA - 1
-             lambda_A <- f_squared_A*(n - PA)
+             f_squared_A <- SSEM/SSEA - 1
+             lambda_A <- f_squared_A*df_A
 
              if (PA - 1 > 0) {
-               F_A <- ((SSE_MEAN - SSEA)/(PA - 1))/(SSEA/(n - PA))
-               p_A <- stats::pf(F_A, (PA - 1), (n - PA), lower.tail = FALSE)
-               power_post_A <- stats::pf(stats::qf(0.95, (PA - 1), (n - PA)), (PA - 1), (n - PA), lambda_A, lower.tail = FALSE)
+               F_A <- ((SSEM - SSEA)/df_A_M)/(SSEA/df_A)
+               p_A <- stats::pf(F_A, df_A_M, df_A, lower.tail = FALSE)
+               power_post_A <- stats::pf(stats::qf(0.95, df_A_M, df_A), df_A_M, df_A, lambda_A, lower.tail = FALSE)
              }
              else {
                F_A <- NA
@@ -154,7 +166,7 @@ compare_lm <- function(fitC=NULL, fitA=NULL, n=NULL, PC=NULL, PA=NULL, SSEC=NULL
   }
   else {
     # fitC and fitA are not given, assign NAs to R-squared
-    SSE_MEAN <- NA
+    SSEM <- NA
 
     PRE_C <- NA
     PRE_C_adj <- NA
@@ -178,20 +190,25 @@ compare_lm <- function(fitC=NULL, fitA=NULL, n=NULL, PC=NULL, PA=NULL, SSEC=NULL
   }
 
   # PRE
+  df_M <- n - 1
+  df_C  <- n - PC
+  df_A  <- n - PA
+  df_A_C <- PA - PC
+
   PRE <- 1 - SSEA/SSEC
   f_squared <- PRE/(1 - PRE)
-  lambda <- f_squared*(n - PA)
-  F <- ((SSEC - SSEA)/(PA - PC))/(SSEA/(n - PA))
-  p <- stats::pf(F, (PA - PC), (n - PA), lower.tail = FALSE)
-  power_post <- stats::pf(stats::qf(0.95, (PA - PC), (n - PA)), (PA - PC), (n - PA), lambda, lower.tail = FALSE)
-  PRE_adj <- 1 - (1 - PRE)*((n - PC)/(n - PA))
+  lambda <- f_squared*df_A
+  F <- ((SSEC - SSEA)/df_A_C )/(SSEA/df_A)
+  p <- stats::pf(F, df_A_C , df_A, lower.tail = FALSE)
+  power_post <- stats::pf(stats::qf(0.95, df_A_C , df_A), df_A_C, df_A, lambda, lower.tail = FALSE)
+  PRE_adj <- 1 - (1 - PRE)*(df_C/df_A)
 
   # Return
   matrix(
-    c(   SSE_MEAN,    n - 1,                        NA,          NA,              NA,    NA,  NA,  NA,        NA,           NA,
-             SSEC,   n - PC,               R_squared_C, f_squared_C, R_squared_adj_C, PRE_C, F_C, p_C, PRE_C_adj, power_post_C,
-             SSEA,   n - PA,               R_squared_A, f_squared_A, R_squared_adj_A, PRE_A, F_A, p_A, PRE_A_adj, power_post_A,
-      SSEC - SSEA,  PA - PC, R_squared_A - R_squared_C,   f_squared,              NA,   PRE,   F,   p,   PRE_adj,   power_post),
+    c(       SSEM,   df_M,                        NA,          NA,              NA,    NA,  NA,  NA,        NA,           NA,
+             SSEC,   df_C,               R_squared_C, f_squared_C, R_squared_adj_C, PRE_C, F_C, p_C, PRE_C_adj, power_post_C,
+             SSEA,   df_A,               R_squared_A, f_squared_A, R_squared_adj_A, PRE_A, F_A, p_A, PRE_A_adj, power_post_A,
+      SSEC - SSEA, df_A_C, R_squared_A - R_squared_C,   f_squared,              NA,   PRE,   F,   p,   PRE_adj,   power_post),
     nrow = 4, ncol = 10, byrow = TRUE,
     dimnames = list(
       c("Baseline", "C", "A", "A vs. C"),
